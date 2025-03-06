40e936ff78beb10c7df46420f8f10f3382399043
Felipe Machado <felpsmachadoo@gmail.com>
2025-03-06 18:02:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,9 +44,9 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="flex justify-center w-screen h-screen p-6 bg-slate-500">
       <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <h1 className="text-3xl font-bold text-center text-slate-100">Gerenciador de Tarefas</h1>
         <AddTask />
         <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
